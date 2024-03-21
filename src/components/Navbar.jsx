816fdaae7c4eec22e9741e31fa4efc7eb518f5b4
Anushree karmakar <anushreekarmakar29@gmail.com>
2024-03-21 14:22:11 +0530

    Add some Ui and fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,9 @@ export default function Navbar() {
     <>
       <div className="flex flex-row justify-between my-2 items-center">
         {/* Logo section  */}
-        <div className=" bg-blue-gray-500 rounded-full md:w-12 md:h-12">
-          <img src="" />
+        <div className=" bg-orange-500 rounded-full md:w-12 md:h-12 flex justify-center items-center">
+          <span className=" font-semibold text-base">Img</span>
+
         </div>
 
         {/* ues ection  */}
@@ -64,15 +65,15 @@ export default function Navbar() {
         </nav>
 
         {/* Auth Links Section */}
-        <nav className="bg-gray-200 p-4 rounded-full">
+        <nav className="bg-green-400 p-4 rounded-full">
           <ul className="flex justify-center">
             <li className="mx-4">
               <NavLink
                 to="/login"
                 className={({ isActive }) =>
                   isActive
-                    ? "ease-in-out hover:underline transition-colors duration-300 hover:text-orange-500 active "
-                    : " text-gray-600 ease-in-out hover:underline transition-colors duration-300 hover:text-orange-500"
+                    ? "ease-in-out hover:underline transition-colors duration-300 hover:text-white active:text-white "
+                    : " text-gray-900 ease-in-out hover:underline transition-colors duration-300 hover:text-white"
                 }
               >
                 Sign In
@@ -83,8 +84,8 @@ export default function Navbar() {
                 to="/signup"
                 className={({ isActive }) =>
                   isActive
-                    ? "ease-in-out hover:underline transition-colors duration-300 hover:text-orange-500 active "
-                    : " text-gray-600 ease-in-out hover:underline transition-colors duration-300 hover:text-orange-500"
+                    ? "ease-in-out hover:underline transition-colors duration-300 hover:text-white active:text-white "
+                    : " text-gray-900 ease-in-out hover:underline transition-colors duration-300 hover:text-white"
                 }
               >
                 Sign Up
--- src/page/Contact.jsx
+++ src/page/Contact.jsx
@@ -3,8 +3,12 @@ import Navbar from "../components/Navbar";
 export default function Contact() {
   return (
     <>
-      <Navbarbar/>
-      <p className="text-xl">Contact us page</p>
+      <div className="md:w-[90%] md:m-auto">
+        <Navbar />
+        <div className=" w-full flex place-content-center ">
+          <p className="text-xl ">Contact us page</p>
+        </div>
+      </div>
     </>
   );
 }
